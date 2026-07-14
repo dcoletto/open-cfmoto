26a238eac7ac55746e8e66068b097f2e46838e87
Dario Coletto <d.coletto@eggonemail.com>
2026-07-15 00:42:23 +0200

    Tagging version 2 (0.2-test)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.coletz.opencfmoto"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2-test"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
